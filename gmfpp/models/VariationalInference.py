c829bb8f2a6a811637ecb17523302231d9382fc9
Sollertis <rasmusml@hotmail.dk>
2022-11-17 14:45:53 +0100

    refactor for easier dev + training

---
+++ gmfpp/models/VariationalInference.py
@@ -0,0 +1,44 @@
+from gmfpp.utils.data_transformers import *
+from torch import nn, Tensor
+import torch
+from typing import List, Set, Dict, Tuple, Optional, Any
+
+
+def reduce(x:Tensor) -> Tensor:
+    """for each datapoint: sum over all dimensions"""
+    flat = view_flat_samples(x)
+    return flat.sum(dim=1)
+
+class VariationalInference(nn.Module):
+    def __init__(self, beta:float=1.):
+        super().__init__()
+        self.beta = beta
+        
+    def forward(self, model:nn.Module, x:Tensor) -> Tuple[Tensor, Dict]:
+        # forward pass through the model
+        outputs = model(x)
+
+        # unpack outputs
+        px, pz, qz, z = [outputs[k] for k in ["px", "pz", "qz", "z"]]
+        
+        # evaluate log probabilities
+        log_px = reduce(px.log_prob(x))
+        log_pz = reduce(pz.log_prob(z))
+        log_qz = reduce(qz.log_prob(z))
+        
+        # compute the ELBO with and without the beta parameter: 
+        # `L^\beta = E_q [ log p(x|z) ] - \beta * D_KL(q(z|x) | p(z))`
+        # where `D_KL(q(z|x) | p(z)) = log q(z|x) - log p(z)`
+        kl = log_qz - log_pz
+        elbo = log_px - kl
+        beta_elbo = log_px - self.beta*kl
+        
+        # loss
+        loss = -beta_elbo.mean()
+        
+        # prepare the output
+        with torch.no_grad():
+            diagnostics = {'elbo': elbo, 'log_px':log_px, 'kl': kl}
+            
+        return loss, diagnostics, outputs
+      
