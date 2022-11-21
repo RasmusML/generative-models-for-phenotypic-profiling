a6d413eaf1999cdec3c530208b054cbdcb52152d
simoneiriksson <simoneiriksson@gmail.com>
2022-11-21 15:02:45 +0100

    Changing datetime to eb "-" separated rather than ":".

--- gmfpp/utils/utils.py
+++ gmfpp/utils/utils.py
@@ -5,7 +5,7 @@ def get_clock_time():
 
 def get_datetime():
     from time import gmtime, strftime
-    result = strftime("%Y-%m-%d - %H:%M:%S", gmtime())
+    result = strftime("%Y-%m-%d - %H-%M-%S", gmtime())
     return result
     
 def cprint(s: str):
