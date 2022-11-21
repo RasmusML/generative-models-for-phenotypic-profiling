2b52a0f624042cce7b637d880d7212347056cd12
simoneiriksson <simoneiriksson@gmail.com>
2022-11-21 11:13:20 +0100

    Added functionality to load and save full models with training and test loss, parameters, vae settings etc.

--- gmfpp/utils/utils.py
+++ gmfpp/utils/utils.py
@@ -2,7 +2,16 @@ def get_clock_time():
     from time import gmtime, strftime
     result = strftime("%H:%M:%S", gmtime())
     return result
+
+def get_datetime():
+    from time import gmtime, strftime
+    result = strftime("%Y-%m-%d - %H:%M:%S", gmtime())
+    return result
     
 def cprint(s: str):
     clock = get_clock_time()
     print("{} | {}".format(clock, s))
+
+
+
+    
