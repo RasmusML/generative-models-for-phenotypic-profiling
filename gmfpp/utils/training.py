40f2da9af841e67c4252638b5ff9cf0af946c083
Sollertis <rasmusml@hotmail.dk>
2022-11-25 22:32:04 +0100

    added code to reconstruction directly from latent representation

--- gmfpp/utils/training.py
+++ gmfpp/utils/training.py
@@ -1,25 +1,29 @@
 import torch
 import numpy as np
 import pandas as pd
+from typing import List, Set, Dict, Tuple, Optional, Any
+from collections import defaultdict
+
+from gmfpp.utils.data_preparation import *
 
 def extract_batch_from_indices(indices: np.ndarray, images: torch.Tensor, metadata: pd.DataFrame) -> Tuple[torch.Tensor, torch.Tensor]:
     batch_images = extract_images_from_metadata_indices(indices, images)
     batch_labels = extract_MOA_ids_from_indices(indices, metadata)
     return batch_images, batch_labels
-    
+
 def get_treatment_indices(metadata: pd.DataFrame) -> Dict[str, list]:
     treatments = np.array(metadata[['Image_Metadata_Compound','Image_Metadata_Concentration']])
     result = defaultdict(list)
-
+    
     for i in range(treatments.shape[0]):
         compound, concetration = treatments[i]
         result[(compound, concetration)] += [i]
-        
+    
     return result
     
 def extract_images_from_metadata_indices(indices: np.ndarray, images: torch.Tensor) -> torch.Tensor:
     return images[indices]
-    
+
 def extract_MOA_ids_from_indices(indices: np.ndarray, metadata: pd.DataFrame) -> torch.Tensor:
     moa_to_id = get_MOA_to_id()
     rows = metadata["moa"].loc[indices]
@@ -47,10 +51,11 @@ class TreatmentBalancedBatchGenerator:
             index = self.treatment_index_at[treatment]
             self.treatment_index_at[treatment] = (index + 1) % len(indices)
             result[i:i+1] = indices[index]
-            
+        
         return result
     
     def next_batch(self) -> Tuple[torch.Tensor, torch.Tensor]:
         indices = self.next_indices()
         X, y = extract_batch_from_indices(indices, self.images, self.metadata)
         return X, y
+
