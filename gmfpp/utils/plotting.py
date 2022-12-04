de9db3a416509f0b76dce05aae4b8f1d02959159
Julia Cathrine Youngman <juca@nnit.com>
2022-12-04 19:01:48 +0100

    julia add cosine plots to plotting.py

--- gmfpp/utils/plotting.py
+++ gmfpp/utils/plotting.py
@@ -1,5 +1,6 @@
 from typing import List, Set, Dict, Tuple, Optional, Any
 import torch
+import numpy as np
 import matplotlib.pyplot as plt
 
 from gmfpp.utils.data_transformers import view_as_image_plot_format, clip_image_to_zero_one
@@ -73,6 +74,48 @@ def plot_VAE_performance(plotdata, file=None, title=None):
         ax.set_ylabel(key)
         ax.set_xlabel("epoch")
         
+    if file == None:
+        plt.show()
+    else: 
+        plt.savefig(file)
+    
+    plt.close()
+
+def plot_cosine_similarity(x0, x1, model, file=None, title=None):
+    #model could be eg. "model_dump/outputs_2022-12-04 - 12-20-15/"
+    #x0.shape and x1.shape should be torch.Size([3, 68, 68])
+    vae, validation_data, training_data, VAE_settings = LoadVAEmodel(model)
+
+    outputs0 = vae(x0[None,:,:,:])
+    outputs1 = vae(x1[None,:,:,:])
+
+    z0 = outputs0["z"].detach().numpy()
+    z1 = outputs1["z"].detach().numpy()
+
+    zs = np.linspace(z0, z1, num=10)
+
+    cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+
+    cp = []
+    for i in range(len(zs)):
+        input1 = vae.observation(zs[i])[0]
+        benchmark = vae.observation(zs[9])[0]
+        cp.append(cos(input1, benchmark).mean().detach().numpy().round(2))
+
+    # create figure
+    fig = plt.figure(figsize=(25, 10))
+
+    # setting values to rows and column variables
+    rows = 1
+    columns = len(zs)
+
+    # Adds a subplot at the 1st position
+    for i in range(columns):
+        fig.add_subplot(rows, columns, i+1)
+        plt.imshow((torch.permute(vae.observation(zs[i])[0], (1, 2, 0))* 255).detach().numpy().astype(np.uint8))
+        plt.axis('off')
+        plt.title(cp[i])
+
     if file == None:
         plt.show()
     else: 
